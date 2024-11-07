68d887f21c58d2ac89bb3acb5fb11c4d92ea1413
david <edwardrfloo@gmail.com>
2024-11-07 17:39:40 -0500

    Se crea el proyecto con los ej de los fundamentos de JavaScript

---
+++ FundamentosJS/recursos/js/Main.js
@@ -0,0 +1,39 @@
+/*Metodo mas usado*/ 
+//document.getElementById("demo").innerHTML="Hello Javascript";
+//document.getElementById("demo").style.fontSize="35px";
+
+/*salidas de datos*/
+//window.alert(7+6);
+console.log(5+6);
+/*Declaracion */
+let a,b,c;
+a=5;
+b=6;
+c=a+b;
+//window.alert(c);
+//document.getElementById("demo").innerHTML=c;
+
+/*Variables */
+let p,q,r;
+p=5;
+q=7;
+const m=8;
+r=p+q+m;
+document.getElementById("demo1").innerHTML=r;
+
+/*Tipo De Datos */
+let num = 24;//numero entero
+let name ="Edward Catamuscay"//cadena
+let km = 14.5;//Numero decimal
+let isColombiano = True;//Booleano
+let obj = {firstname:"Ana",Lastname:"Perez"};//objeto
+
+let y=17+"POO";
+let unir ="17"+"POO";
+window.alert(y);
+window.alert(unir);
+
+/*Definicion de funcion */
+function myFunction(){
+document.getElementById("demo2").innerHTML="New Paragraph";
+}
